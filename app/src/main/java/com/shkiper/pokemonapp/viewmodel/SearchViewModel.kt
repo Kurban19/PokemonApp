254b7492b4b55901bfb8f5ddf344194c7b23b0be
Shkiper01 <baga5828391@gmail.com>
2021-02-28 16:48:39 +0300

    WIP

--- app/src/main/java/com/shkiper/pokemonapp/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/shkiper/pokemonapp/viewmodel/SearchViewModel.kt
@@ -8,6 +8,9 @@ import com.shkiper.pokemonapp.model.Pokemon
 import com.shkiper.pokemonapp.model.Resource
 import com.shkiper.pokemonapp.retrofit.PokeApi
 import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback;
+import retrofit2.Response
 
 
 class SearchViewModel(private val apiHelper: PokeApi): ViewModel() {
@@ -22,6 +25,19 @@ class SearchViewModel(private val apiHelper: PokeApi): ViewModel() {
             try {
                 val resultFromApi = apiHelper.searchPokemon(name = query)
 //                movies.postValue(Resource.success(resultFromApi))
+                resultFromApi.enqueue(object :Callback<List<Pokemon>>(){
+                    override fun onResponse(
+                        call: Call<List<Pokemon>>,
+                        response: Response<List<Pokemon>>
+                    ) {
+                        movies.postValue(Resource.success(response.body()))
+                    }
+
+                    override fun onFailure(call: Call<List<Pokemon>>, t: Throwable) {
+                        movies.postValue(Resource.error(t.localizedMessage.toString(), null))
+                    }
+
+                })
             } catch (e: Exception) {
                 movies.postValue(Resource.error(e.toString(), null))
             }
