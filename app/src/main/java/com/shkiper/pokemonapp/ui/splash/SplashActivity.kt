2991849a924ca51a9065b83ba5a26a97ac4bbe95
Shkiper01 <baga5828391@gmail.com>
2021-03-03 22:09:45 +0300

    add SplashActivity.kt

---
+++ app/src/main/java/com/shkiper/pokemonapp/ui/splash/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.shkiper.pokemonapp.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shkiper.pokemonapp.R
+import com.shkiper.pokemonapp.ui.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+
+//        if(FirebaseAuth.getInstance().currentUser == null){
+//            val intent = Intent(this, LoginActivity::class.java)
+//            startActivity(intent)
+//        }
+//        else{
+//            startActivity(Intent(this, MainActivity::class.java))
+//        }
+//        finish()
+    }
+
+}
