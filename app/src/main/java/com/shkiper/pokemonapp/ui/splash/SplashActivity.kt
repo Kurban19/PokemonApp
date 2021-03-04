691e10e6671dc811e82b28e34f0143e9136b7b5c
Shkiper01 <baga5828391@gmail.com>
2021-03-04 19:59:53 +0300

    add splash icon

--- app/src/main/java/com/shkiper/pokemonapp/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/shkiper/pokemonapp/ui/splash/SplashActivity.kt
@@ -9,7 +9,6 @@ import com.shkiper.pokemonapp.ui.main.MainActivity
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         startActivity(Intent(this, MainActivity::class.java))
 
 //        if(FirebaseAuth.getInstance().currentUser == null){
